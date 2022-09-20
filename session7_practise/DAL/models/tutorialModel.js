fadf67d71596ec570838969eb13822117f147c35
Thành <huuthanh@metechvn.com>
2022-09-20 20:58:50 +0700

    CRUD tutorials

--- session7_practise/DAL/models/tutorialModel.js
+++ session7_practise/DAL/models/tutorialModel.js
@@ -6,12 +6,27 @@ class TutorialModel {
     this.model = mongoose.model("tutorials", TutorialSchema);
   }
   //C - Create
-  create(newTutorial){
-        return this.model.create(newTutorial)
+  create(newTutorial) {
+    return this.model.create(newTutorial);
   }
   //R - Research (All, ByTitle)
+  getAll() {
+    return this.model.find({});
+  }
+  findByTitle(title) {
+    return this.model.find({ title: { $regex: `.*${title}.*` } });
+  }
   //U - Update
+  update(tutorial){
+    return this.model.updateOne(tutorial);
+  }
   //Delete
+  delete(id){
+    return this.model.findByIdAndDelete(id);
+  }
+  deleteAll(){
+    return this.model.deleteMany({})
+  }
 }
 
-module.exports = TutorialModel
+module.exports = TutorialModel;
--- session7_practise/routes/tutorials.js
+++ session7_practise/routes/tutorials.js
@@ -3,8 +3,28 @@ const TutorialModel = require("../DAL/models/tutorialModel");
 var router = express.Router();
 
 var tutorialModel = new TutorialModel();
-
-router.put("/create", (req, res) => {
+router.get("", (req, res) => {
+  tutorialModel
+    .getAll()
+    .then((data) => {
+      res.json({ count: data.length, tutorials: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+router.get("/findByTitle", (req, res) => {
+  let title = req.query.title;
+  tutorialModel
+    .findByTitle(title)
+    .then((data) => {
+      res.json({ count: data.length, toturials: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+router.put("", (req, res) => {
   let newTutorial = req.body;
   tutorialModel
     .create(newTutorial)
@@ -15,5 +35,36 @@ router.put("/create", (req, res) => {
       throw err;
     });
 });
-
+router.post("", (req, res) => {
+  let tutorial = req.body;
+  tutorialModel
+    .update(tutorial)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+router.delete("", (req, res) => {
+  let id = req.query.id;
+  tutorialModel
+    .delete(id)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+router.delete("/deleteAll", (req, res) => {
+  tutorialModel
+    .deleteAll()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
 module.exports = router;
